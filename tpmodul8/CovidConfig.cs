15b52a99fc8687806662df781482f740d495ea13
Aisha Nuryan <91200696+aishaptrn@users.noreply.github.com>
2023-04-09 23:39:18 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8/CovidConfig.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul8
+{
+    internal class CovidConfig
+    {
+        public string satuan_suhu { get; set; }
+        public int batas_hari_demam { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+        public CovidConfig() { }
+        public CovidConfig(string satuan_suhu, int batas_hari_demam, 
+            string pesan_ditolak, string pesan_diterima)
+        {
+            this.satuan_suhu = satuan_suhu;
+            this.batas_hari_demam = batas_hari_demam;
+            this.pesan_ditolak = pesan_ditolak;
+            this.pesan_diterima = pesan_diterima;
+        }
+        
+        public void UbahSatuan_1302213038()
+        {
+            if (satuan_suhu == "celcius")
+            {
+                satuan_suhu = "fahrenheit";
+            } else
+            {
+                satuan_suhu = "celcius";
+            }
+        }
+    }
+
+    class ReadJSON
+    {
+        public CovidConfig covCon;
+        public const string readFile = "D:\\aisha n\\Documents\\AISHA\\uni\\Semester 4\\" +
+            "KPL\\Praktikum\\TP Mod 8\\tpmodul8_1302213038\\tpmodul8\\covid_config.json";
+        public ReadJSON()
+        {
+            try
+            {
+                ReadConfigFile_1302213038();
+            }
+            catch (Exception)
+            {
+                setDefault_1302213038();
+                writeNewConfigFile_1302213038();
+            }
+        }
+
+        private CovidConfig ReadConfigFile_1302213038()
+        {
+            string configJson = File.ReadAllText(readFile);
+            covCon = JsonSerializer.Deserialize<CovidConfig>(configJson);
+            return covCon;
+        }
+
+        private void setDefault_1302213038()
+        {
+            string pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+            string pesan_diterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+            covCon = new CovidConfig("celcius", 14, pesan_ditolak, pesan_diterima);
+        }
+
+        private void writeNewConfigFile_1302213038()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions() { WriteIndented = true };
+
+            string jsonStr = JsonSerializer.Serialize(covCon, options);
+            File.WriteAllText(readFile, jsonStr);
+        }
+    }
+}
---
+++ tpmodul8/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using tpmodul8;
+
+CovidConfig config = new CovidConfig();
+ReadJSON read = new ReadJSON();
+
+read.covCon.UbahSatuan_1302213038();
+
+Console.WriteLine("Berapa suhu badan anda saat ini? " +
+    "Dalam nilai " + read.covCon.satuan_suhu);
+double temp = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Berapa hari yang lalu (perkiraan) " +
+    "anda terakhir memiliki gejala demam?");
+int day = int.Parse(Console.ReadLine());
+
+if (day < read.covCon.batas_hari_demam)
+{
+    if (read.covCon.satuan_suhu == "celcius" 
+        && temp >= 36.5 && temp <= 37.5)
+    {
+        Console.WriteLine(read.covCon.pesan_diterima);
+    } else if (read.covCon.satuan_suhu == "fahrenheit" 
+        && temp >= 97.7 && temp <= 99.5)
+    {
+        Console.WriteLine(read.covCon.pesan_diterima);
+    } else
+    {
+        Console.WriteLine(read.covCon.pesan_ditolak);
+    }
+} else
+{
+    Console.WriteLine(read.covCon.pesan_ditolak);
+}
